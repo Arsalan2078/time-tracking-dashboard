867cff109a079893d34be3135fd6b7ced2ab4117
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-27 10:42:45 -0500

    added scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import css from "./App.module.scss";
+
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <div className={css.app}>
+            <div data-text-preset="1">Deep ocean purple.</div>
+            <div data-text-preset="2">Deep ocean purple.</div>
+            <div data-text-preset="3-regular">Deep ocean purple.</div>
+            <div data-text-preset="3-medium">Deep ocean purple.</div>
+            <div data-text-preset="4">Deep ocean purple.</div>
+        </div>
+    );
 }
